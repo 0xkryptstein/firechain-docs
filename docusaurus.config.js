0a70966a1bfb42a0c63331ecdbc40b4f94003fe4
DONASWAP <info@donaswap.com>
2026-03-31 22:00:37 +0200

    Latest live docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,47 +6,51 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Documentation',
-  tagline: 'Firechain Documentation and Guides',
-  url: 'https://docs.thefirechain.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Donaswap', // Usually your GitHub org/user name.
-  projectName: 'firechain-docs', // Usually your repo name.
+  title: "Documentation",
+  tagline: "Firechain Documentation and Guides",
+  url: "https://docs.thefirechain.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "0xfirechain", // Usually your GitHub org/user name.
+  projectName: "firechain-docs", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      "classic",
+      // '@docusaurus/preset-classic',
+      {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-      }),
+        // // gtag: 'G-WGXVB8BDCW',
+        // googleTagManager: 'GTM-MN62ZGHW',
+        // // // DEPRECATED: Will be passed to @docusaurus/plugin-google-analytics (only enabled when explicitly specified)
+        // googleAnalytics: 'G-WGXVB8BDCW',
+      },
     ],
   ],
 
@@ -55,8 +59,8 @@ const config = {
     ({
       navbar: {
         logo: {
-          alt: 'Logo',
-          src: 'img/logo.png',
+          alt: "Logo",
+          src: "img/logo.png",
         },
         items: [
           // {
@@ -68,65 +72,65 @@ const config = {
           // {to: '/blog', label: 'Blog', position: 'right'},
           // {to: '/ecosystem', label: 'Ecosystem', position: 'right', docId: 'financial-relief'},
           {
-            href: 'https://twitter.com/thefirechain',
-            label: 'Twitter',
-            position: 'right',
+            href: "https://twitter.com/0xfirechain",
+            label: "Twitter",
+            position: "right",
           },
           {
-            href: 'https://github.com/0xfirechain',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/0xfirechain",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Developers',
+            title: "Developers",
             items: [
               {
-                label: 'Introduction',
-                to: '/docs/intro',
+                label: "Introduction",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.com/invite/FVaTXyNnCS',
+                label: "Discord",
+                href: "https://discord.com/invite/FVaTXyNnCS",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/thefirechain',
+                label: "Twitter",
+                href: "https://twitter.com/0xfirechain",
               },
               {
-                label: 'Telegram',
-                href: 'https://t.me/thefirechain',
+                label: "Telegram",
+                href: "https://t.me/firestationcex",
               },
             ],
           },
           {
-            title: 'Network',
+            title: "Network",
             items: [
               {
-                label: 'Faucet',
-                href: 'https://faucet.thefirechain.com',
+                label: "Faucet",
+                href: "https://faucet.thefirechain.com",
               },
               {
-                label: 'Explorer',
-                href: 'https://firescan.io',
+                label: "Explorer",
+                href: "https://firescan.io",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/0xfirechain',
+                label: "GitHub",
+                href: "https://github.com/0xfirechain",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Firechain Network.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Firechain`,
       },
       prism: {
         theme: lightCodeTheme,
